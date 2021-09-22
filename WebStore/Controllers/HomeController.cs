fcb787ebb8a9ec32a1e855fbe24be769af377dc0
Валерия Будаева <valeria.budaeva@yandex.ru>
2021-09-22 23:59:06 +0300

    Начало работы над добавлением оставшихся вьюшек

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -13,5 +13,13 @@ namespace WebStore.Controllers
         {
             return View();
         }
+        public IActionResult Blog()=>View();
+        public IActionResult BlogSingle()=>View();
+        public IActionResult NotFound404() => View();
+        public IActionResult Cart() => View();
+        public IActionResult CheckOut() => View();
+        public IActionResult Login() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Shop() => View();
     }
 }
