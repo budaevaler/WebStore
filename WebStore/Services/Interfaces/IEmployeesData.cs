33f47d79853369a69d0a62d9d9356e33390387dc
Валерия Будаева <valeria.budaeva@yandex.ru>
2021-09-26 21:05:36 +0300

    Интерфейс сервиса сотрудника

---
+++ WebStore/Services/Interfaces/IEmployeesData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Services.Interfaces
+{
+    interface IEmployeesData
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        int Add(Employee employee);
+        void Update(Employee employee);
+        bool Delete(int id);
+    }
+}
