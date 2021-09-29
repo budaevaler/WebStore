d0886493d18d1849b6c986d8a7edcc9e2687059f
Валерия Будаева <valeria.budaeva@yandex.ru>
2021-09-29 21:55:56 +0300

    Добавлено промежуточное ПО, позволяющее осуществлять связь с браузером

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -32,6 +32,7 @@ namespace WebStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
 
             app.UseStaticFiles();
